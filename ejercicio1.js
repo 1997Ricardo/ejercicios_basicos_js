59af6c46a6d08b2bc95606ef624e94721d0362bc
1997Ricardo <ricardosunarodriguez@gmail.com>
2025-04-10 13:37:50 +0200

    Ejercicio 1 y 2

---
+++ ejercicio1.js
@@ -0,0 +1,16 @@
+let miSuperheroeFavorito = "Hulk";
+let num = 27;
+let altura = 5;
+let longitud = 10;
+let suma = altura + longitud ;
+
+console.log(miSuperheroeFavorito);
+console.log(num);
+console.log(altura);
+console.log(longitud);
+console.log(suma);
+
+
+
+
+
---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+const jedi = {nombre: "Luke Skywalker", edad: 19};
+
+// Tu código para cambiar la edad aquí...
+
+let nombre = "Luke Skywalker";
+let edad = 25;
+
+const jedi2 = {nombre , edad};
+console.log(jedi2)
+
