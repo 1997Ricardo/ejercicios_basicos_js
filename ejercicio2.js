3586669653ca71ab059ace39cdf66eaf41c825df
1997Ricardo <ricardosunarodriguez@gmail.com>
2025-04-11 17:51:32 +0200

    ejercicio 4 listo

--- ejercicio2.js
+++ ejercicio2.js
@@ -11,3 +11,29 @@ const apellido = "Organa";
 const edad = 20;
 
 console.log("Soy " + nombre + " " + apellido + ", tengo " + edad + " años y soy una princesa de Alderaan");
+
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+
+// Tu código para calcular el precio total aquí...
+const precioTotal= sable1.precio + sable2.precio;
+
+
+console.log("El precio total de los sables es " + precioTotal + "€");
+
+
+let precioBaseGlobal = 10000;
+
+// Cambia el precio base en esta linea modificando la variable
+
+const nave1 = {nombre: "Ala-X", precioBase: 50000, precioFinal: 60000};
+const nave2 = {nombre: "Halcón Milenario", precioBase: 70000, precioFinal: 80000};
+
+// Tu código para actualizar el precio final de cada nave aquí...
+precioBaseGlobal = 25000;
+
+nave1.precioFinal = precioBaseGlobal + nave1.precioBase;
+nave2.precioFinal = precioBaseGlobal + nave2.precioBase;
+
+console.log("El precio final actualizado de las dos naves es: " + nave1.nombre + " " + nave1.precioFinal + " créditos y " + nave2.nombre + " " + nave2.precioFinal + " créditos");
+
---
+++ ejercicio3.js
@@ -0,0 +1,25 @@
+// 3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+const multiplica = 10 * 5;
+console.log(multiplica);
+
+// 3.2 Divide 10 por 2 y muestra el resultado en un console.
+const dividir = 10 / 2;
+console.log(dividir);
+
+// 3.3 Muestra mediante un console el resto de dividir 15 por 9.
+const resto = 15 % 9;
+console.log(resto);
+
+// 3.4 Usa el correcto operador de asignación que resultará en o = 15,
+// teniendo dos variables p = 10 y j = 5.
+const p = 10;
+const j = 5;
+const o = p + j;
+console.log("o = " + o);
+
+/*3.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.*/
+const c = 10;
+const m = 5;
+const i = c * m;
+console.log("i = " + i);
